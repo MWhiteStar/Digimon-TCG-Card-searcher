054473cff6caddf1d55972b8b420e7528507a6df
Marcus Matheus <37487333+MWhiteStar@users.noreply.github.com>
2025-04-04 16:54:54 -0300

    Buscas Refinadas, Filtros Adicionados, Limite de cartas por pagina adicionado, Possibilidade de pesquisar Cartas sem digitar nada liberado. somente o filtro de raridade de carta não está funcionando, será arrumado no proximo commit.

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 
+let allCards = [];
+let currentPage = 1;
+let cardsPerPage = 30;
+//valores base da pagina atual e quantas cartas por pagina.
+
+async function searchCard(page = 1){
+    currentPage = page;
+    mostrarPaginaAtual();
 
 
-async function searchCard(){
-    
     //variavel cardName, vai pegar o valor do nome digitado no campo input.
     const cardNameInput = document.getElementById('cardNameInput');
-    const cardName = cardNameInput.value;
-        //pesquisa o valor de cardName dentro da url da Api
-   // console.log('Valor de cardName:', cardName);
+    const cardName = cardNameInput.value.trim();
+    //pesquisa o valor de cardName dentro da url da Api
 
-   if(!cardName){
-    alert("Digite o nome de uma carta!");
-    return;
-   }
 
-   const apiUrl =`https://digimoncard.io/api-public/search.php?n=${encodeURIComponent(cardName)}&desc=include`; //
+    const rarity = document.getElementById('rarity').value; //filtro de raridade não esta funcionando, estudar depois o por que.
+    const color = document.getElementById('cardColor').value;
+    const type = document.getElementById('tipoDeCarta').value;
+    const level = document.getElementById('level').value;
+       //filtros de pesquisa.
+
+
+   
+   let apiUrl =`https://digimoncard.io/api-public/search.php?exact=true`; //buscas refinada para retornar somente cartas iguais ao pesquisado.
+
+   //adicionando os filtros a pesquisa
+   if(cardName) apiUrl += `&n=${encodeURIComponent(cardName)}`;
+   if(rarity) apiUrl += `&rarity=${encodeURIComponent(rarity)}`;
+   if(color) apiUrl += `&color=${encodeURIComponent(color)}`;
+   if(level) apiUrl += `&level=${encodeURIComponent(level)}`;
+   if(type) apiUrl += `&type=${encodeURIComponent(type)}`;
+
+   console.log("URL da API gerada:", apiUrl); // Debug para ver a URL gerada
 
     const digiList = document.getElementById('digimonCardInfo');
     digiList.innerHTML = ""; //para limpar resultados anteriores.
@@ -23,26 +41,45 @@ async function searchCard(){
 {
     //define a variavel resposta que é igual a esperar a busca de informações do nome do card na api
     const response = await fetch(apiUrl);
-    
-   
+      
             if (!response.ok) {
                 throw new Error(`Erro na solicitação da API. Status: ${response.status}`);
             }
-            //data apenas transforma a resposta em um arquivo json.
+            //A variavel data apenas transforma a resposta em um arquivo json.
             const data = await response.json();
-            console.log("Dados retornados pela API:", data);
+            //console.log("Dados retornados pela API:", data); somente para ajudar no debug
 
             if(data.length === 0){
                 digiList.innerHTML = "<p>Nenhuma carta encontrada.</p>";
                 return;
             }
 
-            data.forEach(digimon => {
+            allCards = data;        
+            atualizarPagina(data.length);
+            mostrarPaginaAtual(); //renderiza o resultado na tela.
+
+
+        }catch(error){
+            console.error("Erro ao buscar a carta:", error);
+            digiList.innerHTML = "<p>Erro ao buscar a carta. Tente novamente.</p>";
+        }
+    }
+                function mostrarPaginaAtual(){
+                if (!allCards.length) return; //evita que a função seja chamada antes de allCards seja preenchido
+
+                const digiList = document.getElementById('digimonCardInfo');
+                digiList.innerHTML = "";
+                
+                const start = (currentPage - 1) * cardsPerPage;
+                const end = start + cardsPerPage;
+                const paginaCartas = allCards.slice(start, end);
+
+                paginaCartas.forEach(digimon => {
                 const cardDiv = document.createElement("div");
                 cardDiv.classList.add("imagem");
 
                 const img = document.createElement("img");
-                img.src = `https://images.digimoncard.io/images/cards/${digimon.id}.jpg` || "fallback-image.jpg";
+                img.src = digimon.img_url || `https://images.digimoncard.io/images/cards/${digimon.id}.jpg` || "fallback-image.jpg";
                 img.classList.add("imagemDigi");
                 img.alt = digimon.name || "Digimon Card";
 
@@ -55,9 +92,9 @@ async function searchCard(){
 
                 const infoText = document.createElement("p");
                 infoText.classList.add("cartasInfo");
-                infoText.innerHTML = (digimon.main_effect ? `<b>Efeito principal </b>${digimon.main_effect}` : "Sem efeito") + 
-                 "<br>" + 
-                 (digimon.source_effect ? `<b>Efeito de Herança: </b>${digimon.source_effect}` : "sem efeito de herança");
+                infoText.innerHTML = (digimon.main_effect ? `<b>Efeito principal </b>${digimon.main_effect}` : "<b>Sem efeito</b>") + 
+                 "<br>" +  "<br>" +
+                 (digimon.source_effect ? `<b>Efeito de Herança: </b>${digimon.source_effect}` : "<b>sem efeito de herança</b>");
 
                 infoDiv.appendChild(title);
                 infoDiv.appendChild(infoText);
@@ -65,14 +102,32 @@ async function searchCard(){
                 cardDiv.appendChild(infoDiv);
                 digiList.appendChild(cardDiv);
 
-            });
+            });}
 
+    function atualizarPagina(resultadoTotal){
+        const paginaDiv =   document.getElementById("pagina");
+        paginaDiv.innerHTML = "";
 
-        }catch(error){
-            console.error("Erro ao buscar a carta:", error);
-            digiList.innerHTML = "<p>Erro ao buscar a carta. Tente novamente.</p>";
+        if(currentPage > 0){
+            const prevButton = document.createElement("button");
+            prevButton.textContent = "< Pagina Anterior.";
+            prevButton.onclick = () => searchCard(currentPage - 1);
+            paginaDiv.appendChild(prevButton);
+        }
+
+        if(allCards.length > currentPage * cardsPerPage){
+            const nextButton = document.createElement("button");
+            nextButton.textContent = "Proxima pagina >";
+            nextButton.onclick = () => searchCard(currentPage + 1);
+            paginaDiv.appendChild(nextButton);
         }
     }
+
+    function alterarLimite(novoLimite){
+        cardsPerPage = parseInt(novoLimite);
+        searchCard(1);
+    };
+    
 
             //decidi tentar o metodo a cima para melhor otimização do codigo.
 
@@ -103,9 +158,9 @@ async function searchCard(){
 //};
 
 function limpaText(){
+  document.getElementById('rarity').value = "";
+  document.getElementById('cardColor').value = "";
   document.getElementById('cardNameInput').value = "";
+  document.getElementById('level').value= "";
+  document.getElementById('tipoDeCarta').value ="";
 };
-
-fetch("https://digimoncard.io/api-public/search.php?n=Agumon")
-    .then(response => response.json())
-    .then(data => console.log("Data 2 retornada", data));
